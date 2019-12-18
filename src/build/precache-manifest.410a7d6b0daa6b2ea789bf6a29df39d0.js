0328c0efadfc70f4d5a8d78015e3e089e24ea156
Shadman Hossain <shadman@Leidoss-MBP-3.fios-router.home>
2019-12-18 17:42:13 -0500

    first commit

---
+++ src/build/precache-manifest.410a7d6b0daa6b2ea789bf6a29df39d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a62b962d37f334304c16cd787b7fafb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e62d490c22c25d8e601",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "3879c2051e5ad70d002a",
+    "url": "/static/js/2.a2d3916f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.a2d3916f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e62d490c22c25d8e601",
+    "url": "/static/js/main.0f775396.chunk.js"
+  },
+  {
+    "revision": "6e55ea343e46b8691e3d",
+    "url": "/static/js/runtime-main.b5470c2c.js"
+  }
+]);
